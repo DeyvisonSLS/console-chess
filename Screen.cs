86b1dffab76002add588f3afafc471b8a5366707
Deyvison Silas <deyvisonsilas122@gmail.com>
2019-09-11 02:45:58 -0300

    Handling possible errors  in entering wrong chessposition.

--- Screen.cs
+++ Screen.cs
@@ -6,6 +6,7 @@ namespace xadrez_console
 {
     class Screen
     {
+        //  Printa o gameboard sem o auxílio da matriz de movimentos possíveis, sem alterar cor de fundo
         public static void PrintGameBoard(GameBoard gboard)
         {
             for(int i = 0; i < gboard.Lines; i++)
@@ -24,9 +25,11 @@ namespace xadrez_console
             ConsoleColor black = Console.BackgroundColor;
             ConsoleColor gray = ConsoleColor.DarkGray;
 
+            //  Print das linhas (vertical)
             for(int i = 0; i < gboard.Lines; i++)
             {
                 Console.Write(8 - i + " ");
+                //  Print das colunas (horizontal)
                 for(int j = 0; j < gboard.Collumns; j++)
                 {
                     if(possiblePositions[i,j])
@@ -39,17 +42,47 @@ namespace xadrez_console
                     }
                     PrintPiece(gboard.GetPiece(i,j));
                 }
+                //  Se a última posição da coluna permanecer em gray, as próximas casas terão o fundo gray até que o print das colunas ache uma
+                //  casa que tenha o valor false na matriz possiblePositions[] e volte para black
+                //
+                //  Resetando o fundo para black sempre que sair da impressão das colunas (impressão horizontal)
                 Console.BackgroundColor = black;
                 Console.WriteLine();
             }
             Console.WriteLine("  a b c d e f g h");
+            //  Último reset do fundo para black antes de sair da impressão do tabuleiro
             Console.BackgroundColor = black;
         }
         public static ChessPosition ReadChessPosition()
         {
+
             string s = Console.ReadLine();
-            char collumn = s[0];
-            int line = int.Parse(s[1] + "");
+            char collumn;
+            int line;
+            if(s.Length != 2)
+            {
+                throw new GameBoardException("Looks like you entered a wrong origin.");
+            }
+            //  Checking the s[0] type; it needs to be a value that can be parsed to char.
+            bool isChar = char.TryParse(s[0] + "", out char c);
+            if(isChar)
+            {
+                collumn = c;
+            }
+            else
+            {
+                throw new GameBoardException("Looks like you entered a wrong origin.");
+            }
+            //  Checking s[1] type; it needs to be a value that can be parsed to Int.
+            bool isNumeric = int.TryParse(s[1] + "", out int n); //  + "" no lugar de .ToString()
+            if(isNumeric)
+            {
+                line = n;
+            }
+            else
+            {
+                throw new GameBoardException("Looks like you entered a wrong origin.");
+            }
             return new ChessPosition(collumn, line);
         }
         public static void PrintPiece(Piece piece)
