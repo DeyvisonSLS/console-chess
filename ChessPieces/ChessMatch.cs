ac739896334e2dcc2a1dc2d4414345739247f177
Deyvison Silas <deyvisonsilas122@gmail.com>
2019-11-15 14:01:46 -0300

    Pieces organized

--- ChessPieces/ChessMatch.cs
+++ ChessPieces/ChessMatch.cs
@@ -248,27 +248,44 @@ namespace ChessPieces
         }
         //  Here the chessmatch organize the initial pieces set up.
         public void PutPieces()
-        {
-            PutNewPiece('a', 8, new King(Gboard, Color.Black));
-            PutNewPiece('b', 8, new Tower(Gboard, Color.Black));
-
-            PutNewPiece('d', 1, new King(Gboard, Color.White));
-            PutNewPiece('c', 1, new Tower(Gboard, Color.White));
-            PutNewPiece('h', 7, new Tower(Gboard, Color.White));
-
-            // PutNewPiece('c', 1, new Tower(Gboard, Color.White));
-            // PutNewPiece('c', 2, new Tower(Gboard, Color.White));
-            // PutNewPiece('d', 2, new Tower(Gboard, Color.White));
-            // PutNewPiece('e', 2, new Tower(Gboard, Color.White));
-            // PutNewPiece('e', 1, new Tower(Gboard, Color.White));
-            // PutNewPiece('d', 1, new King(Gboard, Color.White));
-
-            // PutNewPiece('c', 8, new Tower(Gboard, Color.Black));
-            // PutNewPiece('c', 7, new Tower(Gboard, Color.Black));
-            // PutNewPiece('d', 7, new Tower(Gboard, Color.Black));
-            // PutNewPiece('e', 7, new Tower(Gboard, Color.Black));
-            // PutNewPiece('e', 8, new Tower(Gboard, Color.Black));
-            // PutNewPiece('d', 8, new King(Gboard, Color.Black));
+        {  
+            //  Black pieces
+            PutNewPiece('a', 8, new Rook(Gboard, Color.Black));
+            PutNewPiece('b', 8, new Knight(Gboard, Color.Black));
+            PutNewPiece('c', 8, new Bishop(Gboard, Color.Black));
+            PutNewPiece('d', 8, new Queen(Gboard, Color.Black));
+            PutNewPiece('e', 8, new King(Gboard, Color.Black));
+            PutNewPiece('f', 8, new Bishop(Gboard, Color.Black));
+            PutNewPiece('g', 8, new Knight(Gboard, Color.Black));
+            PutNewPiece('h', 8, new Rook(Gboard, Color.Black));
+            //  Black pawn
+            PutNewPiece('a', 7, new Pawn(Gboard, Color.Black));
+            PutNewPiece('b', 7, new Pawn(Gboard, Color.Black));
+            PutNewPiece('c', 7, new Pawn(Gboard, Color.Black));
+            PutNewPiece('d', 7, new Pawn(Gboard, Color.Black));
+            PutNewPiece('e', 7, new Pawn(Gboard, Color.Black));
+            PutNewPiece('f', 7, new Pawn(Gboard, Color.Black));
+            PutNewPiece('g', 7, new Pawn(Gboard, Color.Black));
+            PutNewPiece('h', 7, new Pawn(Gboard, Color.Black));
+            //
+            //  White pieces
+            PutNewPiece('a', 1, new Rook(Gboard, Color.White));
+            PutNewPiece('b', 1, new Knight(Gboard, Color.White));
+            PutNewPiece('c', 1, new Bishop(Gboard, Color.White));
+            PutNewPiece('d', 1, new Queen(Gboard, Color.White));
+            PutNewPiece('e', 1, new King(Gboard, Color.White));
+            PutNewPiece('f', 1, new Bishop(Gboard, Color.White));
+            PutNewPiece('g', 1, new Knight(Gboard, Color.White));
+            PutNewPiece('h', 1, new Rook(Gboard, Color.White));
+            //  White pawn
+            PutNewPiece('a', 2, new Pawn(Gboard, Color.White));
+            PutNewPiece('b', 2, new Pawn(Gboard, Color.White));
+            PutNewPiece('c', 2, new Pawn(Gboard, Color.White));
+            PutNewPiece('d', 2, new Pawn(Gboard, Color.White));
+            PutNewPiece('e', 2, new Pawn(Gboard, Color.White));
+            PutNewPiece('f', 2, new Pawn(Gboard, Color.White));
+            PutNewPiece('g', 2, new Pawn(Gboard, Color.White));
+            PutNewPiece('h', 2, new Pawn(Gboard, Color.White));
         }
     }
 }
